9932222c458a033236e6fbe330a50deaea292549
IamNirmata <work.harified@gmail.com>
2025-12-07 12:29:52 -0800

    Fix path in 8-pre-launch.sh

--- llm/setup_and_data/8-pre-launch.sh
+++ llm/setup_and_data/8-pre-launch.sh
@@ -1,8 +1,7 @@
-cd /workspace/distrbuted_training_tools
-git pull origin main
-cd /workspace/distrbuted_training_tools/llm_finetune/setup_and_data
+cd /data/Cluster-Validation-Runbook/llm/setup_and_data
+# git pull origin main
 echo "Starting pre-launch setup and data scripts..."
-echo "make sure the secrets are set by running: source ../../../secrets.sh"
+# echo "make sure the secrets are set by running: source ../../../secrets.sh"
 bash 0-setup.sh
 # bash 1-data.sh
 # bash 2-model.sh
