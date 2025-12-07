1e6f3662de73e34507e50eac192c16a8b0fbde39
IamNirmata <work.harified@gmail.com>
2025-12-07 13:33:09 -0800

    Fix path in server.sh

--- llm/setup_and_data/server.sh
+++ llm/setup_and_data/server.sh
@@ -34,5 +34,5 @@ mpirun \
         --node_rank=$NODE_RANK \
         --master_addr=$MASTER_ADDR \
         --master_port=$MASTER_PORT \
-        /opt/Cluster-Validation-Runbook/llm/setup_and_data/fsdp_no4.py
+        /data/Cluster-Validation-Runbook/llm/setup_and_data/fsdp_no4.py
     '
