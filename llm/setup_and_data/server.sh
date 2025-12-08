8a236b7bd61c6ea54abfcc079318e2c7d20a7150
IamNirmata <work.harified@gmail.com>
2025-12-08 09:08:46 -0800

    Auto commit at 20251208_090846

--- llm/setup_and_data/server.sh
+++ llm/setup_and_data/server.sh
@@ -27,9 +27,9 @@ mpirun \
     bash -c '
     export NODE_RANK=$OMPI_COMM_WORLD_RANK
     export MASTER_ADDR="'$MASTER_ADDR'"
-    export MASTER_PORT=12345
-    export NNODES=$(echo ${VC_SERVER_HOSTS//,/ } ${VC_CLIENT_HOSTS//,/ } | wc -w)
-    export WORLD_SIZE=$(( NNODES * 8 ))
+    export MASTER_PORT="'$MASTER_PORT'"
+    export NNODES='$NNODES'
+    export WORLD_SIZE='$WORLD_SIZE'
     export MODEL_PATH="/opt/llm/models/Meta-Llama-3-8B-Instruct"
     export DATASET_PATH="/opt/llm/datasets/xlam-function-calling-60k"
     export OUTPUT_DIR="/data/llm/output/llama-3-8b-function-calling-fsdp-no4"
@@ -40,12 +40,19 @@ mpirun \
     export PYTHONUNBUFFERED=1
 
     echo "Node $(hostname): Starting FSDP No.4 (Rank $NODE_RANK)..."
+    echo " torchrun command: torchrun \
+        --nproc_per_node=8 \
+        --nnodes=$NNODES \
+        --node_rank=$NODE_RANK \
+        --master_addr= $MASTER_ADDR \
+        --master_port=$MASTER_PORT \
+        /data/Cluster-Validation-Runbook/llm/setup_and_data/fsdp_no4.py"
 
     torchrun \
         --nproc_per_node=8 \
         --nnodes=$NNODES \
         --node_rank=$NODE_RANK \
-        --master_addr=$MASTER_ADDR \
+        --master_addr= $MASTER_ADDR \
         --master_port=$MASTER_PORT \
         /data/Cluster-Validation-Runbook/llm/setup_and_data/fsdp_no4.py
     '
