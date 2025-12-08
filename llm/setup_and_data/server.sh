5a9d7415d706943bbba70af1938c8a9ad41a8a6f
IamNirmata <work.harified@gmail.com>
2025-12-08 15:12:32 -0800

    Auto commit at 20251208_151232

--- llm/setup_and_data/server.sh
+++ llm/setup_and_data/server.sh
@@ -50,7 +50,6 @@ echo "WORLD_SIZE: $WORLD_SIZE"
 mkdir -p /data/llm/results
 timestamp=$(date +%Y%m%d_%H%M%S)
 mkdir -p /data/llm/results/llama3_finetune_$timestamp
-
 mpirun \
   --allow-run-as-root \
   --hostfile "$HOSTFILE" \
@@ -91,7 +90,7 @@ mpirun \
       --master_addr=$MASTER_ADDR \
       --master_port=$MASTER_PORT \
       /data/Cluster-Validation-Runbook/llm/setup_and_data/fsdp70b.py
-  '
+  ' 2>&1 | tee /data/llm/results/llama3_finetune_$timestamp/finetune_$timestamp.log
 
 
 
