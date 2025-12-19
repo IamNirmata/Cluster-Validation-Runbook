77077cacdee2bda6e862ece44e1e5cc9dd380c02
IamNirmata <work.harified@gmail.com>
2025-12-19 13:52:50 -0800

    Auto commit at 20251219_135250

--- storage-tests/run.sh
+++ storage-tests/run.sh
@@ -3,5 +3,5 @@ echo "GCRNODE: $GCRNODE"
 #california timrstamp
 timestamp=$(date +%Y%m%d_%H%M%S -d 'TZ="America/Los_Angeles" now')
 echo "Timestamp: $timestamp"
-mkdir -p /data/storage-tests/$GCRNODE
-echo "Storage test directory set up at /data/storage-tests/$GCRNODE"
+mkdir -p /data/storage-tests/$GCRNODE/$timestamp
+echo "Storage test directory set up at /data/storage-tests/$GCRNODE/$timestamp"
