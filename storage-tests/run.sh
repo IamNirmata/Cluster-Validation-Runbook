b534e513e726bfbc9c549333b120c069ec2affd4
IamNirmata <work.harified@gmail.com>
2025-12-19 13:56:43 -0800

    Auto commit at 20251219_135643

--- storage-tests/run.sh
+++ storage-tests/run.sh
@@ -11,7 +11,8 @@ echo "Storage test directory set up at /data/storage-tests/$GCRNODE/$timestamp"
 numjobs read nfiles test
 
 fio numjobs_read_nfiles.fio --output-format=json --output=/data/storage_test/output/numjobs_read_nfiles.json
-sdsd
+
+
 
 
 
