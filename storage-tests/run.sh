0e78e70a13df849e60e32cb2c4156a3f49832f79
IamNirmata <work.harified@gmail.com>
2025-12-19 14:12:24 -0800

    Auto commit at 20251219_141224

--- storage-tests/run.sh
+++ storage-tests/run.sh
@@ -34,7 +34,7 @@ fio randwrite.fio --output-format=json --output=/data/storage_test/output/randwr
 #write test then read test
 echo "Starting storage tests..."
 echo "Running random write test...1/6 tests"
-fio randwrite.fio --output-format=json --output=/data/storage-tests/$GCRNODE/$timestamp/randwrite.json
+fio randwrite.fio --output-format=json --output=/data/storage-tests/out$GCRNODE/$timestamp/randwrite.json
 echo "Running random read test...2/6 tests"
 fio randread.fio --output-format=json --output=/data/storage-tests/$GCRNODE/$timestamp/randread.json
 
