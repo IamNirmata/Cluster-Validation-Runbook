0024d62d0e1fad32ec3fac7866efba8437b499bf
IamNirmata <work.harified@gmail.com>
2025-12-19 14:12:33 -0800

    Auto commit at 20251219_141233

--- storage-tests/run.sh
+++ storage-tests/run.sh
@@ -45,6 +45,6 @@ fio iodepth_read_1file.fio --output-format=json --output=/data/storage_tests/out
 echo "Running numjobs write test...5/6 tests"
 fio numjobs_write_nfiles.fio --output-format=json --output=/data/storage_tests/output/$GCRNODE/$timestamp/numjobs_write_nfiles.json
 echo "Running numjobs read test...6/6 tests"
-fio numjobs_read_nfiles.fio --output-format=json --output=/data/storage-tests/$GCRNODE/$timestamp/numjobs_read_nfiles.json
+fio numjobs_read_nfiles.fio --output-format=json --output=/data/storage_tests/output/$GCRNODE/$timestamp/numjobs_read_nfiles.json
 
-echo "Storage tests completed. Results saved in /data/storage-tests/$GCRNODE/$timestamp/"
+echo "Storage tests completed. Results saved in /data/storage_tests/output/$GCRNODE/$timestamp/"
