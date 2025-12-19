62d8496a67b4a1e16be524132d363e10361a93f6
IamNirmata <work.harified@gmail.com>
2025-12-19 13:57:27 -0800

    Auto commit at 20251219_135727

--- storage-tests/run.sh
+++ storage-tests/run.sh
@@ -33,6 +33,8 @@ Random read test
 fio randread.fio --output-format=json --output=/data/storage_test/output/randread.json
 
 
+Random write test
 
+fio randwrite.fio --output-format=json --output=/data/storage_test/output/randwrite.json
 
 """
