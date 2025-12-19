61d01129a52745a00d24b5f9b10f72685f5b14fa
IamNirmata <work.harified@gmail.com>
2025-12-19 14:08:01 -0800

    Auto commit at 20251219_140801

--- storage-tests/run.sh
+++ storage-tests/run.sh
@@ -42,4 +42,5 @@ fio randwrite.fio --output-format=json --output=/data/storage-tests/$GCRNODE/$ti
 fio randread.fio --output-format=json --output=/data/storage-tests/$GCRNODE/$timestamp/randread.json
 fio iodepth_write_1file.fio --output-format=json --output=/data/storage-tests/$GCRNODE/$timestamp/iodepth_write_1file.json
 fio iodepth_read_1file.fio --output-format=json --output=/data/storage-tests/$GCRNODE/$timestamp/iodepth_read_1file.json
-fio numjobs_write_nfiles.fio --output-format=json --output=/data/storage-tests/$GCRNODE/$timestamp/numjobs_write_n
+fio numjobs_write_nfiles.fio --output-format=json --output=/data/storage-tests/$GCRNODE/$timestamp/numjobs_write_nfiles.json
+fio numjobs_read_nfiles.fio --output-format=json --output=/data/storage
